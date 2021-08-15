a5333864f8a1eb57a8fb58b7eb7a5e23a4a419e6
Finn <sdv7178@autuni.ac.nz>
2021-08-15 15:11:18 +1200

    final commit

---
+++ frontend/src/components/Dropdown.js
@@ -0,0 +1,16 @@
+import SEPractices from "../dummydata/SEPractices.js"
+
+const optionItems = SEPractices.map((SEPractice) =>
+<option key={SEPractice.practice}>{SEPractice.practice}</option>
+);
+const Dropdown = () => {
+    return (
+        <div>
+            <select>
+                <option value="">Select an SE Practice</option>
+                {optionItems}
+            </select>
+        </div>
+    )
+}
+export default Dropdown;
